767ac9fb457f72b36c68b917a845197c36c5623e
bala <vbalakumar.cse@gmail.com>
2026-06-25 01:28:17 -0500

    fix

--- lib/infra-cdk-stack.ts
+++ lib/infra-cdk-stack.ts
@@ -15,7 +15,7 @@ export class TempleAppInfraCdkStack extends Stack {
     super(scope, id, props);
     var project = "temple-";
     var tableNames: string[] = [];
-    const corsOrigins: string[] = ["http://192.168.1.160:3000", "http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "http://localhost:3003", "https://dev-htky.templehub.org", "https://htky.templehub.org", "https://sbht.templehub.org", "https://htky.org", "https://www.htky.org", "https://templehub.org", "https://dev.templehub.org", "https://qa.templehub.org", "https://www.templehub.org"];
+    const corsOrigins: string[] = ["http://192.168.1.160:3000", "http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "http://localhost:3003", "https://dev-htky.templehub.org", "https://dev-sbht.templehub.org", "https://htky.templehub.org", "https://sbht.templehub.org", "https://htky.org", "https://www.htky.org", "https://templehub.org", "https://dev.templehub.org", "https://qa.templehub.org", "https://www.templehub.org"];
     ////..................SQS QUEUES................./////////
     var s3BucketName = "temple";
     if (`${cdk.Stack.of(this).region}` == "us-east-1") {
